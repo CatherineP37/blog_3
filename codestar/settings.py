26bf1c43bcffae7fef46fbc0c95e09675f597d17
Catherine Parrish <cathp27@gmail.com>
2024-02-18 14:38:07 +0000

    Add Comment class

--- codestar/settings.py
+++ codestar/settings.py
@@ -91,7 +91,8 @@ DATABASES = {
 
 CSRF_TRUSTED_ORIGINS = [
     "https://8000-catherinep37-blog3-kcbs0z6mpbx.ws-eu108.gitpod.io/",
-    "https://blog-33-45003fbda412.herokuapp.com/"
+    "https://blog-33-45003fbda412.herokuapp.com/",
+    "https://8000-catherinep37-blog3-kcbs0z6mpbx.ws-eu108.gitpod.io"
 ]
 
 
